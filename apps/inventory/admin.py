badcfc524b21760108dffc1ca55fb9adda577fe6
danial sadri <danielsadri01@gmail.com>
2024-07-31 15:52:04 +0330

    update

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -5,3 +5,5 @@ from .models import StockRecord
 @admin.register(StockRecord)
 class StockRecordAdmin(admin.ModelAdmin):
     list_display = ['product', 'sku', 'buy_price', 'sale_price', 'num_stock', 'threshold_low_stack']
+    search_fields = ['product__title']
+    raw_id_fields = ['product']
